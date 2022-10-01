578d1171c5807b59fc2379a7329c208a8e329acd
Vima <billajean43@gmail.com>
2022-10-01 15:57:23 -0400

    Add Next.js

--- src/components/Subject.js
+++ src/components/Subject.js
@@ -1,17 +1,23 @@
 import React  from 'react';
 import './Subject.css'
+import { navigate } from "@reach/router"
 
 /**
  * 
- * @param {country} country 
+ * 
  * @returns 
  */
 const Subject = (props) =>{
-    
+    const name = props.SubjectName
+    const move = () => {
+        navigate(`/subjects/${name}`)
+        
+    }
+
     return(
-            <div className='u-flex Subject-container'>
+            <div className='u-flex Subject-container' onClick={move}>
                 <div className='Subject-text'>
-                    {props.subject}   
+                    {name}   
                 </div>
 
             </div>
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -6,13 +6,17 @@ const MainPage = () => {
     document.title = "Main Page";
   
     return (
-        <div className="MainPage-container" >
-           
-            <Input></Input>
-            
-           
-
-        </div>
+        <div class="wrapper">
+            <header class="MainPage-header">
+               <h1> EdPod </h1>
+            </header>
+                <main class="MainPage-container">
+                    <Input></Input>
+                     </main>
+                <footer class="MainPage-footer">
+                     <p> Footer </p>
+                    </footer>
+                    </div>
         
     );
 
