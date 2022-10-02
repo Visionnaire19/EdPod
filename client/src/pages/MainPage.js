2ad39d49d0da8132d60100335589f49dc96460be
qudush2 <95336060+qudush2@users.noreply.github.com>
2022-10-02 06:00:43 -0400

    more changes

--- client/src/pages/MainPage.js
+++ client/src/pages/MainPage.js
@@ -10,28 +10,63 @@ const MainPage = () => {
       <div className="wrapper">
         <div className="upper">
           <p className="slogan">Connecting learners beyond the classroom</p>
-        <input className="textBox"></input>
-        <div className="subtext">
-          <p>Enter the subject or course you are currently working on. Or click on one below to get started.</p>
-        </div>
+          <input className="textBox"></input>
+          <div className="subtext">
+            <p>
+              Enter the subject or course you are currently working on. Or click on one below to get
+              started.
+            </p>
+          </div>
         </div>
         <div className="bubbles">
-        <Bubbles subject="Calculus 1" color="#FFC44D" href="/subjects/Calculus"/>
-        <Bubbles subject="Calculus 2" color="#FFC44D" />
-        <Bubbles subject="Differential Equation" color="#FFC44D" />
-        <Bubbles subject="Linear Algebra" color="#FFC44D" />
-        <Bubbles subject="Python" color="#4D88FF" />
-        <Bubbles subject="Java" color="#4D88FF" />
-        <Bubbles subject="C" color="#4D88FF" />
-        <Bubbles subject="HTML/CSS" color="#4D88FF" />
-        <Bubbles subject="JavaScript" color="#4D88FF" />
-        <Bubbles subject="Biology" color="#31CE7E" />
-        <Bubbles subject="Chemistry" color="#31CE7E" />
-        <Bubbles subject="Physics" color="#31CE7E" />
-        <Bubbles subject="English" color="#D295FF" />
-        <Bubbles subject="French" color="#D295FF" />
-        <Bubbles subject="German" color="#D295FF" />
-        <Bubbles subject="Spanish" color="#D295FF" />
+          <a href="/subjects/Calculus">
+            <Bubbles subject="Calculus 1" color="#FFC44D" href="/subjects/Calculus" />
+          </a>
+          <a href="/subjects/Calculus">
+          <Bubbles subject="Calculus 2" color="#FFC44D" />
+          </a>
+          <a href="/subjects/Differential Equation">
+          <Bubbles subject="Differential Equation" color="#FFC44D" />
+          </a>
+          <a href="/subjects/Linear Algebra">
+          <Bubbles subject="Linear Algebra" color="#FFC44D" />
+          </a>
+          <a href="/subjects/Python">
+          <Bubbles subject="Python" color="#4D88FF" />
+          </a>
+          <a href="/subjects/Java">
+          <Bubbles subject="Java" color="#4D88FF" />
+          </a>
+          <a href="/subjects/C">
+          <Bubbles subject="C" color="#4D88FF" />
+          </a>
+          <a href="/subjects/HTMLCSS">
+          <Bubbles subject="HTML/CSS" color="#4D88FF" />
+          </a>
+          <a href="/subjects/JavaScript">
+          <Bubbles subject="JavaScript" color="#4D88FF" />
+          </a>
+          <a href="/subjects/Biology">
+          <Bubbles subject="Biology" color="#31CE7E" />
+          </a>
+          <a href="/subjects/Chemistry">
+          <Bubbles subject="Chemistry" color="#31CE7E" />
+          </a>
+          <a href="/subjects/Physics">
+          <Bubbles subject="Physics" color="#31CE7E" />
+          </a>
+          <a href="/subjects/English">
+          <Bubbles subject="English" color="#D295FF" />
+          </a>
+          <a href="/subjects/French">
+          <Bubbles subject="French" color="#D295FF" />
+          </a>
+          <a href="/subjects/German">
+          <Bubbles subject="German" color="#D295FF" />
+          </a>
+          <a href="/subjects/Spanish">
+          <Bubbles subject="Spanish" color="#D295FF" />
+          </a>
         </div>
       </div>
     </>
