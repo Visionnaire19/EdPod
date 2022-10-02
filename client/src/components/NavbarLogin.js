76474154e8f30b1408f37db1d56aacaf9efc7a3d
Vima <billajean43@gmail.com>
2022-10-02 06:32:39 -0400

    ...

--- client/src/components/NavbarLogin.js
+++ client/src/components/NavbarLogin.js
@@ -1,25 +1,29 @@
 import React from "react";
 import "./NavbarLogin.css";
 
-const NavBar = () => {
+const NavBar = ({userId}) => {
+  console.log("navbar", userId)
   return (
     <div className="navbar">
       <a href="/" className="underline">
         <header className="pageTitle">EdPod</header>
       </a>
 
-      <div className="buttons">
-        <a href="/signup">
-          <button type="button" className="signUpButton">
-            Sign Up
-          </button>
-        </a>
-        <a href="/login">
-          <button type="button" className="loginButton">
-            Log In
-          </button>
-        </a>
-      </div>
+      {userId ? <button type="button" className="signUpButton"></button>
+      : <div className="buttons">
+      <a href="/signup">
+        <button type="button" className="signUpButton">
+          Sign Up
+        </button>
+      </a>
+      <a href="/login">
+        <button type="button" className="loginButton">
+          Log In
+        </button>
+      </a>
+    </div>}
+
+      
     </div>
   );
 };
