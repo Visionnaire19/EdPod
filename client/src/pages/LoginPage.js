89967ff1bc3ba33790c2de8ae9e263d10ef6221d
awsyb <95385676+awsyb@users.noreply.github.com>
2022-10-02 01:32:42 -0400

    ...

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -9,15 +9,20 @@ import './Forms.css';
 
 
 const LoginPage = () => {
+  function collectLogin () {
+    const username = document.getElementById('Username').value;
+    const password = document.getElementById('Password').value;
+    console.log(email, password);
+  }
     return (
     <div className="wrapper">
         <Form>
-      <Form.Group className="mb-3" controlId="formUsername">
+      <Form.Group className="mb-3" id="Username">
         <Form.Label className="label">Username (4-16 characters)</Form.Label>
         <Form.Control type="text" className="box" placeholder="Username" />
       </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formPassword">
+      <Form.Group className="mb-3" id="Password">
         <Form.Label className="label">Password (8 - 16 characters)</Form.Label>
         <Form.Control type="password" className="box" placeholder="Password" />
       </Form.Group>
